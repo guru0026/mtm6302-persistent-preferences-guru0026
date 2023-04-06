ef2de3455b234c3d937f8b88357c33b6ecbed834
Hari Gurusubramanian <guru0026@algonquinlive.com>
2023-04-06 03:00:34 -0400

    adds list styling

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const shoppingItems = [
     "Cereal"
 ];
 
+const listRow = document.querySelectorAll(`row`);
+
 for (let i = 0; i < shoppingItems.length; i++) {
     const shoppingItem = document.createElement("li");
     shoppingItem.textContent = shoppingItems[i];
@@ -14,9 +16,25 @@ for (let i = 0; i < shoppingItems.length; i++) {
     shoppingList.appendChild(shoppingItem);
 }
 
+const light = document.getElementById(`inlineRadio1`);
+const dark = document.getElementById(`inlineRadio2`);
+const blue = document.getElementById(`inlineRadio3`);
+const html = document.getElementsByTagName(`html`);
+
+light.addEventListener("change", function () {
+    if (dark.checked) {
+        html.setAttribute("data-bs-theme","dark");
+    } else if (blue.checked){
+        html.setAttribute("data-bs-theme","blue");
+    } else {
+        html.setAttribute("data-bs-theme","light");
+    }
+});
+
 const checkNumber = document.getElementById(`inlineCheckbox1`);
 const checkCheckbox = document.getElementById(`inlineCheckbox2`);
 const checkBadge = document.getElementById(`inlineCheckbox3`);
+let span = document.createElement("span");
 
 checkNumber.addEventListener("change", function () {
     if (checkNumber.checked) {
@@ -28,16 +46,19 @@ checkNumber.addEventListener("change", function () {
 
 checkCheckbox.addEventListener("change", function () {
     if (checkCheckbox.checked) {
-        shoppingList.classList.add("list-group-numbered")
+        shoppingList.classList.add("list-group-flush")
     } else {
-        shoppingList.classList.remove("list-group-numbered")
+        shoppingList.classList.remove("list-group-flush")
     }
 });
 
 checkBadge.addEventListener("change", function () {
     if (checkBadge.checked) {
-        shoppingList.classList.add("list-group-numbered")
+        // span.classList.add("badge bg-primary rounded-pill");
+        // span.textContent = "Span text";
+        // shoppingItem.appendChild(span);
+        shoppingList.classList.add("list-group-horizontal")
     } else {
-        shoppingList.classList.remove("list-group-numbered")
+        shoppingList.classList.remove("list-group-horizontal")
     }
 });
