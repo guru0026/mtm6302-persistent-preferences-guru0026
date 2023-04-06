691ab8e98b4d0ae26163e25e91afe924fd9324dc
Hari Gurusubramanian <guru0026@algonquinlive.com>
2023-04-06 00:06:41 -0400

    updates preference menu items

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ for (let i = 0; i < shoppingItems.length; i++) {
     shoppingItem.textContent = shoppingItems[i];
     shoppingList.appendChild(shoppingItem);
 }
+
+const radioLight = document.getElementById(`inlineRadio1`);
+const radioDark = document.getElementById(`inlineRadio2`);
+const radioBlue = document.getElementById(`inlineRadio3`);
+
+radio1.addEventListener("change", function () {
+    if (radioBlue.checked) {
+        shoppingList.classList.add("")
+    }
+})
